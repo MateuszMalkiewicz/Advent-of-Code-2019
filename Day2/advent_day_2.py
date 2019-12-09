211d058ebd74fbad482c62fb7d0119506bee487b
Mateusz Małkiewicz <mathew.malkiewicz@gmail.com>
2019-12-10 00:21:55 +0100

    refactoring of day 2

--- Day2/advent_day_2.py
+++ Day2/advent_day_2.py
@@ -5,58 +5,63 @@ class IntcodeInterpreter:
     def __init__(self, file_name):
         self.file_name = file_name
         self.intcode = self.load_file(file_name)
-        self.instruction_pointer = 0
+        self.instruction_index = 0
         self.opcode = {1: 'add', 2: 'multiply', 99: 'end'}
         self.instruction_length = 4
 
     def load_file(self, file_name):
-        intcode = open(file_name, 'r').readline().split(',')
-        intcode = [int(value) for value in intcode]
+        intcode = list(map(lambda x: int(x), open(file_name, 'r').readline().split(',')))
         return intcode
 
     def reset_intcode(self):
         self.intcode = self.load_file(self.file_name)
-        self.instruction_pointer = 0
+        self.instruction_index = 0
 
     def check_if_in_range(self, instruction_address):
         if instruction_address < len(self.intcode):
             return True
 
     def check_for_end(self):
-        if self.check_if_in_range(self.instruction_pointer):
-            opcode = self.opcode[self.intcode[self.instruction_pointer]]
+        if self.check_if_in_range(self.instruction_index):
+            opcode = self.opcode[self.intcode[self.instruction_index]]
             continue_reading = False if opcode == 'end' else True
             return continue_reading
         else:
             return False
 
-    def find_variables_addresses(self):
-        var_one_address = self.intcode[self.instruction_pointer+1]
-        var_two_address = self.intcode[self.instruction_pointer+2]
-        return var_one_address, var_two_address
+    def find_args_addresses(self):
+        arg_one_address = self.intcode[self.instruction_index+1]
+        arg_two_address = self.intcode[self.instruction_index+2]
+        return arg_one_address, arg_two_address
 
-    def find_variables(self):
-        variable_addresses = self.find_variables_addresses()
-        return self.intcode[variable_addresses[0]], self.intcode[variable_addresses[1]]
+    def find_arguments(self):
+        args_addresses = self.find_args_addresses()
+        return self.intcode[args_addresses[0]], self.intcode[args_addresses[1]]
 
-    def instruction_result(self):
-        variables = self.find_variables()
-        opcode = self.opcode[self.intcode[self.instruction_pointer]]
+    def opcode_result(self):
+        arguments = self.find_arguments()
+        opcode = self.opcode[self.intcode[self.instruction_index]]
+        return self.opcode_interpreter(opcode, arguments)
+
+    @staticmethod
+    def opcode_interpreter(opcode, arguments):
         if opcode == 'add':
-            return sum(variables)
+            return sum(arguments)
         elif opcode == 'multiply':
-            return prod(variables)
+            return prod(arguments)
+        else:
+            pass
 
-    def result_destination(self):
-        return self.intcode[self.instruction_pointer+3]
+    def result_destination_address(self):
+        return self.intcode[self.instruction_index+3]
 
     def process_opcode(self):
-        self.intcode[self.result_destination()] = self.instruction_result()
+        self.intcode[self.result_destination_address()] = self.opcode_result()
 
     def process_intcode(self):
         while self.check_for_end():
             self.process_opcode()
-            self.instruction_pointer += self.instruction_length
+            self.instruction_index += self.instruction_length
 
     def show_intcode(self):
         print(self.intcode)
@@ -75,6 +80,7 @@ class IntcodeInterpreter:
 
 
 advent_intcode = IntcodeInterpreter('input.txt')
+advent_intcode.show_intcode()
 advent_intcode.process_intcode()
 advent_intcode.show_intcode()
 print(advent_intcode.find_inputs_from_result(19690720))
---
+++ Day5/advent_day_5.py
@@ -0,0 +1,2 @@
+from Day2.advent_day_2 import IntcodeInterpreter
+
